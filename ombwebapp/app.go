3e6e68284ec5b6ea84096f68c8700153068c74f1
Nick Skelsey <nskelsey@gmail.com>
2015-04-03 01:10:11 -0400

    Added a verbose mode

--- ombwebapp/app.go
+++ ombwebapp/app.go
@@ -14,7 +14,15 @@ import (
 var (
 	host       = flag.String("host", "localhost:1055", "The ip and port for the server to listen on")
 	staticpath = flag.String("staticpath", "./", "The path to the static files to serve")
+	verbose    = flag.Bool("verbose", false, "Logs the output of every request")
 )
+
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	flag.Parse()
@@ -31,9 +39,16 @@ func main() {
 	api := ahimsarest.Handler(prefix, db)
 
 	mux := http.NewServeMux()
+
 	mux.Handle(prefix, api)
 	mux.Handle("/", http.FileServer(http.Dir(*staticpath)))
 
 	log.Printf("webserver listening at %s.\n", *host)
-	log.Fatal(http.ListenAndServe(*host, mux))
+
+	if *verbose {
+		logger := Log(mux)
+		log.Fatal(http.ListenAndServe(*host, logger))
+	} else {
+		log.Fatal(http.ListenAndServe(*host, mux))
+	}
 }
